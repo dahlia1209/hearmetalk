4e92dffdac273cd8e487d068d08134c227722cad
Ryu Nakamura <dahlia1209@gmail.com>
2023-12-03 23:52:52 +0900

    20231203 2352

--- frontend/src/services/textToSpeechServices.ts
+++ frontend/src/services/textToSpeechServices.ts
@@ -3,7 +3,7 @@ import { AudioDataDto, AudioData } from "@/models/SpeechToText"
 import { Speaker } from "@/models/TextToSpeech"
 import { getFormattedDate } from "@/utils/dateUtils";
 
-export const submitText = async (audioData: AudioData, speaker: Speaker): Promise<AudioData> => {
+export const submitText = async (audioData: AudioData, speaker: Speaker): Promise<AudioDataDto> => {
     try {
         // DTOへ変換
         const audioDataDto = new AudioDataDto("", 0, "", audioData.mimeType, audioData.fileExtension, audioData.text)
@@ -13,18 +13,20 @@ export const submitText = async (audioData: AudioData, speaker: Speaker): Promis
             withCredentials: true,
         });        
         // レスポンスデータの処理
+        console.log(response)
         const formatedDate=getFormattedDate()
         const filename =`${formatedDate}${response.data.fileExtension}`
         const respnseAudioData = {
             ...audioData,
-            audioFile: AudioDataDto.toFile(response.data.encodedData, response.data.fileExtension, filename),
+            audioFile: AudioDataDto.toFile(response.data.encodedData, response.data.mimeType, filename),
             durationMs: response.data.durationMs,
             fileExtension: response.data.fileExtension,
             mimeType: response.data.mimeType,
             filename: filename
         };
         console.info(respnseAudioData);
-        return respnseAudioData
+        return response.data
+        // return respnseAudioData
 
     } catch (error) {
         console.error('Submit Error:', error);
